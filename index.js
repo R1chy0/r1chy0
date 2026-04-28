6f3ee4264bd7c28c5e37d6450151ffffae4a8502
R1chy0 <148494833+R1chy0@users.noreply.github.com>
2026-04-28 17:12:29 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express")
 const bodyParser = require("body-parser")
-const fetch = require("node-fetch")
 
 const app = express()
 app.use(bodyParser.json())
@@ -14,20 +13,17 @@ app.post("/payhip", async (req, res) => {
 
         if (!userId) return res.sendStatus(400)
 
-        await fetch(
-            `https://apis.roblox.com/datastores/v1/universes/${UNIVERSE_ID}/standard-datastores/datastore/WhitelistPlayers/entries/entry`,
-            {
-                method: "POST",
-                headers: {
-                    "x-api-key": API_KEY,
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({
-                    key: userId.toString(),
-                    value: true
-                })
-            }
-        )
+        await fetch(`https://apis.roblox.com/datastores/v1/universes/${UNIVERSE_ID}/standard-datastores/datastore/WhitelistPlayers/entries/entry`, {
+            method: "POST",
+            headers: {
+                "x-api-key": API_KEY,
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                key: userId.toString(),
+                value: true
+            })
+        })
 
         res.sendStatus(200)
     } catch (err) {
